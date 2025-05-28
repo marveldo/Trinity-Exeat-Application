3b067c2e21ab0c464233c668ee9530954bd5e064
utibe solomon <utibesolomon6@gmail.com>
2025-05-28 03:37:32 +0100

    Commited

--- Backend/Exeatapp/settings.py
+++ Backend/Exeatapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qodv$uxy7r*=xhn7kh5fugj_^df=+$acqh()%9hsyn51zr_gg^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
